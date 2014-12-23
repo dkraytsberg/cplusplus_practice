b9d2b3256fdd588a4d41c530b9930ae9fde11328
Dave <dkraytsberg@gmail.com>
2014-12-22 23:07:12 -0500

    work on priority queue and node

--- src/priority_q.cpp
+++ src/priority_q.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include <string>
-
-
- #include "h/file.h"
 
 using namespace std;
 
@@ -28,28 +24,60 @@ public:
       ptr = tmp;
       max *= 2;
     }
+    //sort
+    for(int i = 0; i < size; i++){
+      if(t > ptr[i]){
+        T temp = ptr[i];
+        ptr[i] = t;
+        t = temp;
+      }
+    }
 
     ptr[size++] = t;
   }
   T remove(){
     if(size > 0)
-      return ptr[size--];
+      return ptr[--size];
+
+  }
+  bool contains(T t){
+    for(int i = 0; i < size; i++){
+      if(ptr[i] == t)
+        return true;
+      else if(ptr[i] < t)
+        break;
+    }
+    return false;
   }
   void print(){
-    for(int i = 0; i < size; i++)
+    for(int i = size - 1; i >= 0; i--)
       cout << ptr[i] << endl;
   }
 };
 
+struct node{
+  enum {REG,BLK,STR,END};
+  int type, x, y, f, g, h;
+  node(int x, int y): x(x),y(y),type(REG) {}
+  node(int x, int y, int type): x(x),y(y),type(type) {}
+  
+  bool operator> (node other){ return (f > other.f ? true : false); }
+  bool operator< (node other){ return (f < other.f ? true : false); }
+  bool operator== (node other){
+    return ((other.x == x && other.y == y) ? true : false); }
+  bool operator+ (int add){ return f+add; }
+  bool operator- (int subt){ return f-subt; }
+
+};
+
+
+
 
 int main(){
-  // priority_q<int> q;
-  // for(int i = 1; i < 22; i++){
-  //   q.add(i*i);
-  //   cout << q.max << " " << q.size << endl;
-  // }
-  // q.print();
-  cout << x << endl;
+  priority_q<int> q;
+
+
+
 
 
 
